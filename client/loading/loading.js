b763ff5cf2297ea4388355f71d477dc3994aae5a
bazubrick <seregi123@mail.ru>
2016-01-22 11:44:49 +0200

    fix logo and name

--- client/loading/loading.js
+++ client/loading/loading.js
@@ -3,9 +3,9 @@ Template.loading.rendered = function () {
 var spinner = '<div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>';
   if ( ! Session.get('loadingSplash') ) {
     this.loading = window.pleaseWait({
-      logo: 'ghostPreload.png',
-      backgroundColor: '#414141',
-      loadingHtml: '<p class="loading-message">SCRIPT SPIRIT</p>' + spinner /*+ '<div class="sk-spinner sk-spinner-rotating-plane"></div>'*/
+      logo: 'air.png',
+      backgroundColor: '#12A3D4',
+      loadingHtml: '<p class="loading-message">flycode</p>' + spinner /*+ '<div class="sk-spinner sk-spinner-rotating-plane"></div>'*/
     });
     Session.set('loadingSplash', true); // just show loading splash once
   }
